af6d44bc633dc4b39f4d8071df74eb398cd1824f
ahcombs <aidancombs@gmail.com>
2022-01-08 16:57:21 -0500

    standardize_option function and tests

--- R/dict_eq_utils.R
+++ R/dict_eq_utils.R
@@ -158,7 +158,7 @@ check_key <- function(key){
 #'
 #' @return TRUE if available; throws error otherwise
 check_component <- function(component){
-  components_avail <- c("behavior", "modifier", "identity", "setting", "b", "beh", "m", "mod", "i", "ident", "s", "set", "behaviors", "modifiers", "settings", "identities")
+  components_avail <- c("behavior", "modifier", "identity", "setting", "b", "beh", "m", "mod", "i", "ident", "s", "set", "behaviors", "modifiers", "settings", "identities", "behaviour", "behaviours")
   for(c in component){
     if(!(c %in% unlist(components_avail))){
       stop(message = paste0("Invalid component '", c,"' provided. Valid components are identity, behavior, modifier, and setting."))
@@ -190,4 +190,40 @@ check_gender <- function(gender){
   return(TRUE)
 }
 
-# TODO write a function for standardizing inputs (so if someone provides an abbreviation, convert to input needed for filtering) and tie to above check functions perhaps
+#' standardize_option
+#'
+#' This function deals with abbreviations in parameter specification and returns the spellings that are used in the datasets.
+#'
+#' @param input the string to standardize
+#' @param param the type expected (gender, component, or stat)
+#'
+#' @return
+standardize_option <- function(input, param){
+  for(i in 1:length(input)){
+    if(param == "gender"){
+      input[i] <- dplyr::case_when(substr(input[i], 1, 1) == "m" ~ "male",
+                                   substr(input[i], 1, 1) == "a" ~ "average",
+                                   substr(input[i], 1, 1) %in% c("f", "w") ~ "female",
+                                   TRUE ~ input[i])
+    } else if(param == "component"){
+      input[i] <- dplyr::case_when(substr(input[i], 1, 1) == "b" ~ "behavior",
+                                   substr(input[i], 1, 1) == "m" ~ "modifier",
+                                   substr(input[i], 1, 1) == "i" ~ "identity",
+                                   substr(input[i], 1, 1) == "s" ~ "setting",
+                                   TRUE ~ input[i])
+    } else if(param == "stat"){
+      input[i] <- dplyr::case_when(substr(input[i], 1, 1) == "m" ~ "mean",
+                                   substr(input[i], 1, 1) == "s" ~ "sd",
+                                   substr(input[i], 1, 1) == "c" ~ "cov",
+                                   substr(input[i], 1, 1) == "a" ~ "all",
+                                   TRUE ~ input[i])
+    } else (
+      stop("Invalid parameter type provided.")
+    )
+  }
+  return(input)
+}
+
+
+
+
--- tests/testthat/test-dict_eq_utils.R
+++ tests/testthat/test-dict_eq_utils.R
@@ -1,7 +1,8 @@
+# TODO FINISH TESTS FOR THESE FUNCTIONS
+# ALSO: ANYWHERE ELSE IN THE CODE WHERE I AM CHECKING USER INPUT, CHANGE TO THESE HELPER FUNCTIONS
+
 test_that("search_dict works", {
   expect_equal(2 * 2, 4)
-  # TODO FINISH TESTS FOR THESE FUNCTIONS
-  # ALSO: ANYWHERE ELSE IN THE CODE WHERE I AM CHECKING USER INPUT, CHANGE TO THESE HELPER FUNCTIONS
 })
 
 test_that("check_key works", {
@@ -15,3 +16,14 @@ test_that("check_component works", {
 test_that("check_gender works", {
   expect_equal(2 * 2, 4)
 })
+
+test_that("standardize_option works", {
+  expect_equal(standardize_option("m", "gender"), "male")
+  expect_equal(standardize_option("f", "gender"), "female")
+  expect_equal(standardize_option("a", "gender"), "average")
+  expect_equal(standardize_option(c("male", "man", "woman"), "gender"), c("male", "male", "female"))
+  expect_equal(standardize_option("behaviour", "component"), "behavior")
+  expect_equal(standardize_option(c("m", "standard deviation", 3), "stat"), c("mean", "sd", "3"))
+  expect_error(standardize_option("m", "not a valid parameter"), "Invalid parameter type provided.")
+})
+
